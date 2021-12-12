79b2fd37c765389789cfec66eaccbf66dacb5472
Пользователь <il.lisnichenko@gmail.com>
2021-12-12 17:44:41 +0200

    Test3

--- Program.cs
+++ Program.cs
@@ -1,31 +1,31 @@
-﻿Console.Write("Введите свое Ax: ");
+﻿Console.Write("Enter cordinate x of dot A: ");
 var Ax = Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите свое Ay: ");
+Console.Write("Enter cordinate y of dot A: ");
 var Ay = Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите свое Bx: ");
+Console.Write("Enter cordinate x of dot B: ");
 var Bx = Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите свое By: ");
+Console.Write("Enter cordinate Y of dot B: ");
 var By = Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите свое Cx: ");
+Console.Write("Enter cordinate x of dot C: ");
 var Cx = Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите свое Cy: ");
+Console.Write("Enter cordinate Y of dot C: ");
 var Cy = Convert.ToDouble(Console.ReadLine());
 
 var AB = (float)Math.Sqrt(Math.Pow((Ax - Bx), 2) + Math.Pow((Ay - By), 2));
 var BC = (float)Math.Sqrt(Math.Pow((Cx - Bx), 2) + Math.Pow((Cy - By), 2));
 var AC = (float)Math.Sqrt(Math.Pow((Ax - Cx), 2) + Math.Pow((Ay - Cy), 2));
 var P = AB + BC + AC; 
-Console.WriteLine($"{AB}");
-Console.WriteLine($"{BC}");
-Console.WriteLine($"{AC}");
+Console.WriteLine($"Length of AB is: {AB}");
+Console.WriteLine($"Length of BC is: {BC}");
+Console.WriteLine($"Length of AC is: {AC}");
 if (Math.Abs(AB - BC) <= 0.001 && Math.Abs(AC - BC) <= 0.001)
-    Console.WriteLine("Равносторонний");
+    Console.WriteLine("Triangle is equilateral");
 else
-    Console.WriteLine("Не равносторонний");
+    Console.WriteLine("Triangle is not equilateral");
 if (Math.Abs(BC - AB) <= 0.001 | Math.Abs(AC - AB) <= 0.001 | Math.Abs(AC - BC) <= 0.001)
-    Console.WriteLine("Равнобедренный");
+    Console.WriteLine("Triangle is isosceles");
 else
-    Console.WriteLine("Не Равнобедренный");
+    Console.WriteLine("Triangle is not isosceles");
 
 var minArray = (new[] { AC, AB, BC })[0];
 var maxArray = (new[] { AC, AB, BC })[0];
@@ -40,9 +40,9 @@ for (int i = 1; i < (new[] { AC, AB, BC }).Length; i++)
 var agsArray = P - minArray - maxArray;
 var m = Math.Pow(maxArray, 2) - Math.Pow(minArray, 2) - Math.Pow(agsArray, 2);
 if (Math.Abs(m) <= 0.001)
-    Console.WriteLine("Прямоугольный");
+    Console.WriteLine("Triangle is rectangular");
 else
-    Console.WriteLine("Не Прямоугольный");
-Console.WriteLine($"{P}");
+    Console.WriteLine("Triangle is not rectangular");
+Console.WriteLine($"Perimeter: {P}");
 for (int i = 0; i * 2 < P; i++)
     Console.WriteLine($"{2 * i}");
